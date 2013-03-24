4ec3db87eddd3bd6a13b93e8b5205855f18a5343
behrooz shabani (everplays) <behrooz@rock.com>
2013-03-24 12:17:04 +0430

    initial commit
    with spork / guard / rspec / postgres configured.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VMan::Application.config.secret_token = '920dfa4a8f057273e968d55dc416b8ce90047c888c67b93fa98589fc4fe86defa5fb464aa93c48ad8542e1ca768d96248a6b2ad6b9df61d8d30843890c3e45af'
