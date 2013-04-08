d0f35fa9e9b75708f1f298fe882ab9634943e7aa
behrooz shabani (everplays) <behrooz@rock.com>
2013-04-08 23:51:08 +0430

    added template model implementation based on spec

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,3 +1,18 @@
 class Template < ActiveRecord::Base
   attr_accessible :description, :name
+
+  after_destroy :remove_image
+
+  validates :name, :presence => true, :length => { :minimum => 3 }
+
+  def image_path
+    Rails.root.join('images', "#{id}.qcow2")
+  end
+
+  private
+
+  def remove_image
+    FileUtils.rm(image_path)
+  end
+
 end
