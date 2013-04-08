c9f0482b652fec46799c34aae176b9dc990e4488
behrooz shabani (everplays) <behrooz@rock.com>
2013-04-09 01:55:00 +0430

    added configuration for allowed images and their mimetype

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+VMan::Application.configure do
+  config.supported_images = ['qcow2']
+  config.image_types = {
+    'qcow2' => 'application/octet-stream'
+  }
+end
+
 # Initialize the rails application
 VMan::Application.initialize!
