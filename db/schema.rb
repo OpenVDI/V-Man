0c2b40f6eb6df958046955ad243c13667d7bc808
behrooz shabani (everplays) <behrooz@rock.com>
2013-04-08 21:59:00 +0430

    created template model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130324113303) do
+ActiveRecord::Schema.define(:version => 20130408162136) do
+
+  create_table "templates", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "format"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
