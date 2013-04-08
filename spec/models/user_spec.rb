25067ec50554edaffcdb9536c7ee586ef2fdeee5
behrooz shabani (everplays) <behrooz@rock.com>
2013-04-08 21:17:14 +0430

    added spec to check role_symbols method of user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe User do
-  # nothing other than devise yet!
+
+  context "devise" do
+    let(:user) { FactoryGirl.build(:user) }
+
+    it "should return roles in sombol" do
+      user.role_symbols.each do |role|
+        role.should be_kind_of(Symbol)
+      end
+    end
+
+  end
+
 end
